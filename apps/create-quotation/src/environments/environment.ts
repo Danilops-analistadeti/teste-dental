16551529c1a36fb13d351cfafc857efb7b4e9b71
Danilo Soares <danilops.analistadeti@gmail.com>
2022-06-07 11:10:01 -0300

    Removendo Enviroments

--- apps/create-quotation/src/environments/environment.ts
+++ apps/create-quotation/src/environments/environment.ts
@@ -1,15 +1,15 @@
 import { makeEnviroment } from './make-environment';
 
-const ENV_BUCKET = `https://esfera-frontend-angular-energy-contracting-dev-bucket.s3.amazonaws.com`;
+const ENV_BUCKET = ``;
 
 const ENV_BUCKET_CREATE_QUOTATION = `${ENV_BUCKET}/create-quotation/remoteEntry.js`;
 const ENV_BUCKET_AUTH = `${ENV_BUCKET}/auth/remoteEntry.js`;
 const ENV_BUCKET_HIRING_PROPOSAL = `${ENV_BUCKET}/hiring-proposal/remoteEntry.js`;
 
-const ENV_PATH = 'https://xrekeaas1h.execute-api.us-east-1.amazonaws.com/dev';
-const ENV_LOGIN = 'https://3ccsodqdua.execute-api.us-east-1.amazonaws.com/dev';
+const ENV_PATH = '';
+const ENV_LOGIN = '';
 const ENV_BACKOFFICE =
-  'https://2hm9xezu1j.execute-api.us-east-1.amazonaws.com/dev';
+  '';
 const PRODUCTION = true;
 
 export const environment = {
--- environments/environment.local.ts
+++ environments/environment.local.ts
@@ -1,14 +1,14 @@
 import { makeEnviroment } from './make-environment';
 
-const ENV_BUCKET = 'http://localhost:';
+const ENV_BUCKET = '';
 
 const ENV_BUCKET_CREATE_QUOTATION = `${ENV_BUCKET}3000/remoteEntry.js`;
 const ENV_BUCKET_AUTH = `${ENV_BUCKET}3300/remoteEntry.js`;
 const ENV_BUCKET_HIRING_PROPOSAL = `${ENV_BUCKET}3100/remoteEntry.js`;
 
-const ENV_PATH = 'https://xrekeaas1h.execute-api.us-east-1.amazonaws.com/dev';
-const ENV_LOGIN = 'https://3ccsodqdua.execute-api.us-east-1.amazonaws.com/dev';
-const ENV_BACKOFFICE = 'https://2hm9xezu1j.execute-api.us-east-1.amazonaws.com/dev';
+const ENV_PATH = '';
+const ENV_LOGIN = '';
+const ENV_BACKOFFICE = '';
 const PRODUCTION = true;
 
 export const environment = {
